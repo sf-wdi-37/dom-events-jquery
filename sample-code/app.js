2c188a71f72a5500db7ca27920ef57801f70b63e
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2016-12-01 11:30:15 -0800

    fix typo in js

--- sample-code/app.js
+++ sample-code/app.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
   console.log("DOM is ready!");
 
   //////////// HELLO SPAN ////////////
-  // var greeting = $('#greeting');
+  var greeting = $('#greeting');
   // greeting.on('mouseover', popUpYay);
   // function popUpYay(event){
   //   alert('Yay!');
   // }
 
   //////////// LINKS ////////////
-  // var $links = $("a"); // every link on the page
+  var $links = $("a"); // every link on the page
   // $links.on("click", function handleClick(event){
   //   //event.preventDefault();
   //   alert("You just clicked a link!");
@@ -19,32 +19,35 @@ $(document).ready(function(){
 
   //////////// KITTEN ////////////
 
-  // var kitten = $("#kitten-pic");
+  var kitten = $("#kitten-pic");
   // kitten.on("click", function (event) {
   //   console.log("inside kitten pic click event handler");
   //   console.log("this: ", this);
-  // 	console.log("target: ", event.target);
+  // 	 console.log("target: ", event.target);
+  //   console.log("target same as this?", this === event.target);
   // });
 
-  // var kitten-containertainer = $("#kitten-container");
+  var kittenContainer = $("#kitten-container");
   //
-  // kitten-containertainer.on("click", function (event) {
-  //   console.log("inside kitten pic click event handler");
+  // kittenContainer.on("click", function (event) {
+  //   console.log("inside kitten container click event handler");
   //   console.log("this: ", this);
   //   console.log("target: ", event.target);
+  //   console.log("target same as this?", this === event.target);
   // });
   //
   // $(document).on("click", function (event) {
-  //   console.log("inside kitten pic click event handler");
+  //   console.log("inside document click event handler");
   //   console.log("this: ", this);
   //   console.log("target: ", event.target);
+  //   console.log("target same as this?", this === event.target);
   // });
 
   //////////// BOX ////////////
 
   // setTimeout to add a delay before execution
   // the box isn't on the page when the DOM is first ready
-  window.setTimeout(addBox, 3000);
+  // window.setTimeout(addBox, 3000);
 
   // doesn't work! too early!
   // $(".box").on("click", toggleLongBox);
